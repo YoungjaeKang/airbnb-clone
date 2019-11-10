d3eb2f871ac1c6e8fbf6a3d5793dbeaa2883e472
Youngjae Kang <kyj8028@hotmail.com>
2019-11-10 11:41:23 +0900

    Homeview Complete !
    class based view vs function based view

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,4 @@ from rooms import views as room_views
 # app_name이랑 urls.py config의 namespace랑 같아야 한다.
 app_name = "core"
 
-urlpatterns = [path("", room_views.all_rooms, name="home")]
+urlpatterns = [path("", room_views.HomeView.as_view(), name="home")]
--- rooms/views.py
+++ rooms/views.py
@@ -1,21 +1,47 @@
-from math import ceil
-from django.shortcuts import render
-from django.core.paginator import Paginator
+# from math import ceil
+from django.utils import timezone
+from django.views.generic import ListView
+
+# from django.shortcuts import render, redirect
+from django.core.paginator import Paginator, EmptyPage
 from . import models
 
-# from datetime import datetime
-# from django.http import HttpResponse
+# class based view와 function based view 사이에 논쟁이 많다.
+class HomeView(ListView):
+
+    """ HomeView Definition """
+
+    # ccbv
+    model = models.Room
+    paginate_by = 10
+    paginate_orphans = 5
+    ordering = "created"
+    context_object_name = "rooms"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        now = timezone.now()
+        context["now"] = now
+        return context
+
+    # page_kwarg = "potato"
 
 
+"""
+2. Django의 도움을 조금만 받기
 def all_rooms(request):
     page = request.GET.get("page", 1)
     room_list = models.Room.objects.all()
     paginator = Paginator(room_list, 10, orphans=5)
-    rooms = paginator.page(int(page))
-    return render(request, "rooms/home.html", {"page": rooms})
 
-    """
-    1. 수동으로 페이지네이션을 구현하는 방법
+    # 없는 페이지의 url을 넣었을 때 home으로 돌려보내고 url을 정상적으로 보이게 한다.
+    try:
+        rooms = paginator.page(int(page))
+        return render(request, "rooms/home.html", {"page": rooms})
+    except EmptyPage:
+        return redirect("/")"""
+
+""" 1. 수동으로 페이지네이션을 구현하는 방법
     page = int(page or 1)
     page_size = 10
     limit = page_size * page
