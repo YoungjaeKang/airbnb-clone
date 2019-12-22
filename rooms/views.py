845a4e15492f1f8dfb0b1e684363081bf795df77
Jay Kang <kyj8028@hotmail.com>
2019-12-23 00:28:06 +0900

    need to fix error!
    UnboundLocalError at /rooms/search/
    
    local variable 'rooms' referenced before assignment

--- rooms/views.py
+++ rooms/views.py
@@ -1,5 +1,5 @@
 # from math import ceil
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, View
 from django.http import Http404
 from django_countries import countries
 
@@ -34,11 +34,79 @@ class RoomDetail(DetailView):
     # 404도 자동으로 띄워준다.
 
 
-def search(request):
-   
-    form = forms.SearchForm()
+class SearchView(View):
+    """ SearchView Definition """
 
-    return render(request, "rooms/search.html", {"form": form})
+    def get(self, request):
+        country = request.GET.get("country")
+
+        # http://127.0.0.1:8000/rooms/search/ 로 들어갔을 때 빈 값에 대한 default를 주기 위한 코드
+        if country:
+            
+            form = forms.SearchForm(request.GET)    # request.GET으로 form은 입력한 검색 값을 기억한다.
+            
+            if form.is_valid():
+
+                # print(form.cleaned_data)
+
+                city = form.cleaned_data.get("city")
+                country = form.cleaned_data.get("country")
+                room_type = form.cleaned_data.get("room_type")
+                price = form.cleaned_data.get("price")
+                guests = form.cleaned_data.get("guests")
+                bedrooms = form.cleaned_data.get("bedrooms")
+                beds = form.cleaned_data.get("beds")
+                baths = form.cleaned_data.get("baths")
+                instant_book = form.cleaned_data.get("instant_book")
+                superhost = form.cleaned_data.get("superhost")
+                amenities = form.cleaned_data.get("amenities")
+                facilities = form.cleaned_data.get("facilities")
+
+                # filter =================================
+                filter_args = {}
+
+                if city != "Anywhere":
+                    filter_args["city__startswith"] = city
+
+                filter_args["country"] = country
+
+                if room_type is not None:
+                    filter_args["room_type"] = room_type
+
+                if price is not None:
+                    filter_args["price__lte"] = price
+
+                if guests is not None:
+                    filter_args["guests__gte"] = guests
+
+                if bedrooms is not None:
+                    filter_args["bedrooms__gte"] = bedrooms
+
+                if beds is not None:
+                    filter_args["beds__gte"] = beds
+
+                if baths is not None:
+                    filter_args["baths__gte"] = baths
+
+                if instant_book is True:
+                    filter_args["instant_book"] = True
+
+                if superhost is True:
+                    filter_args["host__superhost"] = True
+
+                for amenity in amenities:
+                    filter_args["amenities"] = amenity
+
+                for facility in facilities:
+                    filter_args["facilities"] = facility
+
+                rooms = models.Room.objects.filter(**filter_args)
+
+        else:
+            
+            form = forms.SearchForm()
+
+        return render(request, "rooms/search.html", {"form": form, "rooms": rooms})
 
 
 # 2. fbv
